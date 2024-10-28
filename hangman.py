c45fd078163e36297ba928a5c91dbc8716f27248
Vj-pixel <70001381+Vj-pixel@users.noreply.github.com>
2024-10-27 19:23:00 -0700

    added play function; all details are in comments anyway

--- hangman.py
+++ hangman.py
@@ -4,3 +4,26 @@ from words import words
 def get_word():
     word = random.choice(words)
     return word.upper()
+
+
+def play(word):
+    word_completion = "_" * len(word) #number of _'s multiplied by the length of the word
+    guessed = False #false until user guesses it
+    guessed_letters = []
+    guessed_words = []
+    tries = 6 # 2 arms, head, 2 legs, body
+    print("Let's play hangman!")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+    # while loop below is when it's not guessed and tries are greater than 0
+    while not guessed and tries > 0:
+        guess = input("Please guess a letter or word: ").upper() #converts it to uppercase cuz that's how hangman works!
+        # if-else block
+        if len(guess) == 1 and guess.isalpha():
+
+
+        elif len(guess) == len(word) and guess.isalpha():
+
+        else:
+            print("Not a valid guess")
