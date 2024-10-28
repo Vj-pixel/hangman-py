be36d8838545f3befec63a831e8f5a07f01de041
Vj-pixel <70001381+Vj-pixel@users.noreply.github.com>
2024-10-27 19:39:43 -0700

    made a LOT of nested if-else blocks to check if the guess was correct

--- hangman.py
+++ hangman.py
@@ -31,12 +31,28 @@ def play(word):
                 print("Good job,", guess, "is in the word!")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
-
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                word_completion="".join(word_as_list)
+                if "_" not in word_completion:
+                    guessed = True
         elif len(guess) == len(word) and guess.isalpha(): # checks if the guess is a word and it is a alphabetic letter
-
+            if guess in guessed_words:
+                print("You already guessed the word", guess)
+            elif guess != word:
+                print(guess, "is not the word")
+            else:
+                guessed = True
+                word_completion = word
         else:
             print("Not a valid guess")
 
         print(display_hangman(tries))
         print(word_completion)
         print("\n")
+
+    if guessed:
+        print("Congrats, you guessed the word! You win!")
+    else:
+        print("Sorry you ran out of tries. The word was " + word + ". Maybe next time!")
